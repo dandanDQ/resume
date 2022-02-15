f74c00a28e5f76ffb08d7421d57c23d9c1d3022c
dandanDQ <shendanqiong@qq.com>
2022-02-15 19:15:15 +0800

    feat: more preview style.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue(), monacoEditorPlugin()],
   base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
