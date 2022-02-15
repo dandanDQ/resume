00e1f7d9072b6b947c8415d599212723e8c9a013
dandanDQ <shendanqiong@qq.com>
2022-02-15 20:50:46 +0800

    fix: 修复打包问题

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,13 +1,16 @@
 import { defineAsyncComponent } from 'vue'
 
 // 导出组件列表，并注册异步组件
-const files = import.meta.globEager("/src/components/templates/*.vue")
+const files = import.meta.glob("/src/components/templates/*.vue")
 const comList = []
 const components = {}
-for(const file in files) {
+
+Object.keys(files).map((file) => {
   const comName = file.slice(26, -4)
   comList.push(comName)
-  components[comName] = defineAsyncComponent(() => import(file))
-}
+  components[comName] = defineAsyncComponent({
+    loader: files[file]
+  })
+})
 
 export { comList, components } 
