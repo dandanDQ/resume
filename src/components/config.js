d81e2ee4cc480a83382f2d161e34caf9410fa87f
dandanDQ <shendanqiong@qq.com>
2022-06-03 17:22:05 +0800

    feat: 整理index文件

---
+++ src/components/config.js
@@ -0,0 +1,65 @@
+const defaultVal = `name: 填名字
+
+infos: 
+  - xxx: xxx
+
+contact:
+  phone: 填电话
+  email: 填邮箱
+  wechat: 填微信
+  github: 填github
+
+skills:
+- name: JavaScript
+  level: 75
+- name: HTML5
+  level: 75
+- name: CSS3
+  level: 80
+- name: Vue 2.x
+  level: 75
+- name: ReactNative
+  level: 50
+- name: Webpack
+  level: 65
+
+CARD-xx经历（识别开头是CARD-的）:
+- name: 大标题
+  time: 时间时间时间
+  desc:
+   - 描述1: xxx
+   - 描述2: xxx
+- name: 大标题
+  time: 时间时间时间
+  desc:
+   - 描述1: xxx
+   - 描述2: 描述2描述2描述2描述2描述2描述2描述2描述2描述2描述2描述2
+   - 说明: 只有英文key是固定的
+
+CARD-xx经历2（可以随便添加）:
+- name: 大标题
+  time: 时间时间时间
+  desc:
+   - 描述1: xxx
+   - 描述2: xxx
+- name: 大标题
+  time: 时间时间时间
+  desc:
+   - 描述1: 描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述
+   - 描述2: xxx
+   - 说明: 只有英文key是固定的
+
+CARD-xx经历3（可以随便添加）:
+- name: 大标题
+  time: 时间时间时间
+  desc:
+   - 描述1: 描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1
+   - 描述2: xxx
+- name: 大标题
+  time: 时间时间时间
+  desc:
+   - 描述1: xxx
+   - 描述2: xxx
+   - 说明: 只有英文key是固定的`;
+
+export default defaultVal;
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,6 +19,9 @@
           <div class="ui-button" @click="saveHtml">
             saveHtml
           </div>
+          <div class="ui-button" @click="changeEditMode">
+            edit
+          </div>
           <!-- <div class="ui-button" @click="changeEditorList('next')">
             <img src="../assets/icon_next.svg" />
           </div> -->
@@ -55,74 +58,10 @@
 <script>
 import Editor from '@/components/Editor.vue'
 import Title from '@/components/Title.vue'
-
 import { jsPDF } from "jspdf";
 import html2canvas from "html2canvas"; 
 import { comList, components}  from '@/components/utils.js'
-
-const defaulVal = `name: 填名字
-
-infos: 
-  - xxx: xxx
-
-contact:
-  phone: 填电话
-  email: 填邮箱
-  wechat: 填微信
-  github: 填github
-
-skills:
-- name: JavaScript
-  level: 75
-- name: HTML5
-  level: 75
-- name: CSS3
-  level: 80
-- name: Vue 2.x
-  level: 75
-- name: ReactNative
-  level: 50
-- name: Webpack
-  level: 65
-
-CARD-xx经历（识别开头是CARD-的）:
-- name: 大标题
-  time: 时间时间时间
-  desc:
-   - 描述1: xxx
-   - 描述2: xxx
-- name: 大标题
-  time: 时间时间时间
-  desc:
-   - 描述1: xxx
-   - 描述2: 描述2描述2描述2描述2描述2描述2描述2描述2描述2描述2描述2
-   - 说明: 只有英文key是固定的
-
-CARD-xx经历2（可以随便添加）:
-- name: 大标题
-  time: 时间时间时间
-  desc:
-   - 描述1: xxx
-   - 描述2: xxx
-- name: 大标题
-  time: 时间时间时间
-  desc:
-   - 描述1: 描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述
-   - 描述2: xxx
-   - 说明: 只有英文key是固定的
-
-CARD-xx经历3（可以随便添加）:
-- name: 大标题
-  time: 时间时间时间
-  desc:
-   - 描述1: 描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1描述1
-   - 描述2: xxx
-- name: 大标题
-  time: 时间时间时间
-  desc:
-   - 描述1: xxx
-   - 描述2: xxx
-   - 说明: 只有英文key是固定的`
+import defaultVal from '@/components/config.js'
 
 export default {
   name: 'Resume',
@@ -151,7 +90,7 @@ export default {
     }
   },
   created() { 
-    const initVal = JSON.stringify([{ yaml: defaulVal, preview: {}}, { yaml: defaulVal, preview: {}} ])
+    const initVal = JSON.stringify([{ yaml: defaultVal, preview: {}}, { yaml: defaultVal, preview: {}} ])
     const editorString = window.localStorage.getItem('editorList') ?? initVal
     this.editorList = JSON.parse(editorString)
     window.addEventListener('beforeunload', (e) => {
